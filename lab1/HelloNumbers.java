15e81367f392626e2d66ec802bab2c42c37d1e96
liliachenyun <liliachen.chenyun@gmail.com>
2019-02-14 23:40:41 -0800

    completed lab1 3rd version

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,9 +1,12 @@
 public class HelloNumbers {
 	public static void main(String[] args) {
-        int x = 0;
-        while (x < 10) {
-            System.out.print(x + " ");
-            x = x + 1;
+		int sum = 0;
+        for (int i = 0; i < 10; i++ ) {
+        	for (int x = 0; x < i + 1; x++){
+        		sum += x;
+        	}
+            System.out.print(sum + " ");
+            sum = 0;
         }
     }
 }
