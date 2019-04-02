6a0fe1c17858672428cc358960157f0d31fb8c38
liliachenyun <liliachen.chenyun@gmail.com>
2019-04-02 13:05:27 -0700

    completed HW1

---
+++ hw1/es/datastructur/synthesizer/BoundedQueue.java
@@ -0,0 +1,28 @@
+package es.datastructur.synthesizer;
+
+import java.util.Iterator;
+
+public interface BoundedQueue<T> extends Iterable<T> {
+    int capacity();     // return size of the buffer
+    int fillCount();    // return number of items currently in the buffer
+    void enqueue(T x);  // add item x to the end
+    T dequeue();        // delete and return item from the front
+    T peek();           // return (but do not delete) item from the front
+    public Iterator<T> iterator();
+
+    default boolean isEmpty(){
+        if (this.fillCount() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    default boolean isFull(){
+        if (this.fillCount() == this.capacity()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
